7cc636974d504d2137432764c78e03419dc95c5a
Tsutsumi Tomoharu <ttomoharu201@gmail.com>
2020-09-21 15:37:54 +0900

    merge requested users and search

--- app/controllers/users/requests_controller.rb
+++ app/controllers/users/requests_controller.rb
@@ -1,10 +1,5 @@
 class Users::RequestsController < ApplicationController
-  before_action :set_user, only: %i(index new create)
-
-  def index
-    # 送った人一覧と送られて人一覧でparameterで分けて表示でいいような気がする　今は
-    @sent_from_users = current_user.sent_from_users
-  end
+  before_action :set_user, only: %i(new create)
 
   def new; end
 
