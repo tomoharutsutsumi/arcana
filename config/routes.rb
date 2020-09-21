4101a679c76d56cbcd5c5cd3e5ce39388effcc14
Tsutsumi Tomoharu <ttomoharu201@gmail.com>
2020-09-21 11:44:57 +0900

    devise routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+  root 'users/lists#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :users do
     resources :searches
