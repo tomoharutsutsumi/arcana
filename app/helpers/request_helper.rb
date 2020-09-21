1a004cdb41b50d96cf0f321db55fd4e79dac0e11
Tsutsumi Tomoharu <ttomoharu201@gmail.com>
2020-09-21 11:11:37 +0900

    requestedlist view

--- app/helpers/request_helper.rb
+++ app/helpers/request_helper.rb
@@ -1,13 +1,13 @@
 module RequestHelper
-  def find_request_id(sent_from_id, sent_to_id)
-    PermissionRequest.find_by(sent_from_id: sent_from_id, sent_to_id: sent_to_id).id
+  def find_request(sent_from_id, sent_to_id)
+    PermissionRequest.find_by(sent_from_id: sent_from_id, sent_to_id: sent_to_id)
   end
 
   def check_requesting_user_status(current_user, requesting_user)
     status = PermissionRequest.find_by(sent_from_id: requesting_user.id, sent_to_id: current_user.id).status
     if status == PermissionRequest::DEFAULT
-      link_to '承認', user_request_path(u.id, find_request_id(u.id, current_user.id), judge: 'permit'), method: :patch
-      link_to '非承認', user_request_path(u.id, find_request_id(u.id, current_user.id), judge: 'reject'), method: :patch
+      link_to '承認', user_request_path(u.id, find_request(u.id, current_user.id).id, judge: 'permit'), method: :patch
+      link_to '非承認', user_request_path(u.id, find_request(u.id, current_user.id).id, judge: 'reject'), method: :patch
     elsif status == PermissionRequest::PERMITTED
       '承認済み'
     else
