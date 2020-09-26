374ad60c6a800a6e714951f3432a72407887698b
Tsutsumi Tomoharu <ttomoharu201@gmail.com>
2020-09-26 14:26:15 +0900

    unnecessary code delete

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,14 +2,12 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    callback_from :facebook #文字列じゃだめなん
+    callback_from('facebook')
   end
 
   private
 
   def callback_from(provider)
-    provider = provider.to_s
-
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
