5077038f438a66f1a69896a1cc2e56b06a9bafcb
Tsutsumi Tomoharu <ttomoharu201@gmail.com>
2020-09-29 21:06:30 +0900

    fix mypage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_024228) do
+ActiveRecord::Schema.define(version: 2020_09_29_114234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2020_09_22_024228) do
     t.datetime "updated_at", null: false
     t.string "uid"
     t.string "provider"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
