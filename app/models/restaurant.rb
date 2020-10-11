9d74cfc3816402634bbdfdba9cd68fd8762f7a09
Tsutsumi Tomoharu <ttomoharu201@gmail.com>
2020-10-11 19:53:36 +0900

    fix

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,13 @@
 class Restaurant < ApplicationRecord
+  require 'net/http'
   belongs_to :list
   has_many :participants
 
   def self.get_info_from_api(name)
     request_url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=' << ENV['HOTPEPPER_API_KEY'] << "&name=#{name}"
     request_url = URI.encode(request_url)
-    results = Hash.from_xml open(request_url).read
+    request_url = URI.parse(request_url)
+    results = Hash.from_xml Net::HTTP.get(request_url)
     results = results['results']['shop']
   end
 end
