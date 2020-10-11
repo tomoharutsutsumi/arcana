8b1a1ca13e4febe687ccafdc8aee4a822c222c33
Tsutsumi Tomoharu <ttomoharu201@gmail.com>
2020-10-11 15:52:52 +0900

    creating restaurant

--- app/controllers/lists/restaurants_controller.rb
+++ app/controllers/lists/restaurants_controller.rb
@@ -7,13 +7,18 @@ class Lists::RestaurantsController < ApplicationController
 
   def new
     @results = Restaurant.get_info_from_api(params[:name]) if params.has_key?(:name)
-    @restaurant = @list.restaurants.build
   end
 
   def create
-    @restaurant = @list.restaurants.build(restaurant_params)
+    @restaurant = @list.restaurants.build(restaurant_params.merge(
+      name: params[:restaurant][:name],
+      price: params[:restaurant][:budget][:name],
+      place: params[:restaurant][:address],
+      category: params[:restaurant][:genre][:catch],
+      url: params[:restaurant][:urls][:pc],
+    ))
     @restaurant.save
-    redirect_to list_restaurants_path(@list)
+    redirect_to new_list_restaurant_path(@list)
   end
 
   private
@@ -23,6 +28,6 @@ class Lists::RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:url, :price, :comment, :participant, :name, :place, :recommended_menu, :category)
+    params.require(:restaurant).permit(:comment, :participant, :recommended_menu)
   end
 end
