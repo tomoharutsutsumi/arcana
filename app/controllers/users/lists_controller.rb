952880e6f20e73e587e9e11aae4326a683261171
Tsutsumi Tomoharu <ttomoharu201@gmail.com>
2020-10-26 19:45:39 +0900

    delete list

--- app/controllers/users/lists_controller.rb
+++ app/controllers/users/lists_controller.rb
@@ -12,7 +12,13 @@ class Users::ListsController < ApplicationController
   def create
     @list = current_user.lists.build(list_params)
     @list.save
-    redirect_to user_lists_path(current_user)
+    redirect_to user_lists_path(current_user), notice: 'リストを登録しました'
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to user_lists_path(current_user), notice: 'リストを削除しました'
   end
 
   private
