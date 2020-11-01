ba50e6c01bd1c1c7f5dd5f332a10de343a4613fe
Tsutsumi Tomoharu <ttomoharu201@gmail.com>
2020-11-01 16:25:34 +0900

    add information2

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,18 +7,30 @@ class RestaurantsController < ApplicationController
 
   def update
     @restaurant.update(restaurant_params)
-    redirect_to list_restaurants_path(@restaurant.list)
+    redirect_to list_restaurants_path(@restaurant.list), notice: 'お店を編集しました'
   end
 
   def destroy
     @restaurant.delete
-    redirect_to list_restaurants_path(@restaurant.list)
+    redirect_to list_restaurants_path(@restaurant.list), notice: 'お店を削除しました'
   end
 
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:url, :price, :comment, :participant, :name, :place, :recommended_menu, :category)
+    params.require(:restaurant).permit(
+      :url, :comment, :participant, 
+      :name, :recommended_menu, :tel, 
+      :combined_access, :opentime, :holiday,
+    )
+  end
+
+  def additional_restaurant_params
+    {
+      price: params[:restaurant][:budget],
+      place: params[:restaurant][:address],
+      category: params[:restaurant][:category],
+    }
   end
 
   def set_restaurant
