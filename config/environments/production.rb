8c0ffbe7ff73c3696e86a5816aaa977981895aba
Tsutsumi Tomoharu <ttomoharu201@gmail.com>
2021-03-01 20:22:23 +0900

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,16 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "arcana_#{Rails.env}"
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            ENV['ARCANA_MAILER_ADDRESS'], 
+    password:             ENV['ARCANA_MAILER_PASSWORD'],
+    authentication:       'plain',
+  }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
